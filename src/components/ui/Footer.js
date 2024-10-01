9facc2a06790999485fde304c66268efc97850ba
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-10-01 04:53:53 -0700

    Social Media Icon

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -2,6 +2,10 @@ import { styled } from "@mui/system";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 import footerAdornment from "../../assets/Footer Adornment.svg";
+import { Hidden } from "@mui/material";
+import Facebook from "../../assets/facebook.svg";
+import Instagram from "../../assets/instagram.svg";
+import twitter from "../../assets/twitter.svg";
 const StyledFooter = styled("footer")(({ theme }) => ({
   backgroundColor: theme.palette.common.blue,
   width: "100%",
@@ -31,148 +35,192 @@ const StyledGridItems = styled(Grid)(({ theme }) => ({
   fontWeight: "bold",
   textDecoration: "none",
 }));
+const SocialMediaIcon = styled("img")(({ theme }) => ({
+  height: "4em",
+  width: "4em",
+  [theme.breakpoints.down("md")]: {
+    height: "2.5em",
+    width: "2.5em",
+  },
+}));
 const Footer = ({ value, setValue, selectedIndex, setSelectedIndex }) => {
   return (
     <StyledFooter>
       <StyledGridMain container justifyContent="center">
-        <StyledGridMainItem item>
-          <Grid container direction="column" spacing={2}>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/"
-              onClick={() => setValue(0)}
-            >
-              Home
-            </StyledGridItems>
-          </Grid>
-        </StyledGridMainItem>
-        <StyledGridMainItem item>
-          <Grid container direction="column" spacing={2}>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/services"
-              onClick={() => {
-                setValue(1);
-                setSelectedIndex(0);
-              }}
-            >
-              Services
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/customSoftware"
-              onClick={() => {
-                setValue(1);
-                setSelectedIndex(1);
-              }}
-            >
-              Custom Software Development
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/mobileApp"
-              onClick={() => {
-                setValue(1);
-                setSelectedIndex(2);
-              }}
-            >
-              Mobile App Development
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/websites"
-              onClick={() => {
-                setValue(1);
-                setSelectedIndex(3);
-              }}
-            >
-              Websites Development
-            </StyledGridItems>
-          </Grid>
-        </StyledGridMainItem>
-        <StyledGridMainItem item>
-          <Grid container direction="column" spacing={2}>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/theRevolution"
-              onClick={() => setValue(2)}
-            >
-              The Revolution
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/vision"
-              onClick={() => setValue(2)}
-            >
-              Vision
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/technology"
-              onClick={() => setValue(2)}
-            >
-              Technology
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/process"
-              onClick={() => setValue(2)}
-            >
-              Process
-            </StyledGridItems>
-          </Grid>
-        </StyledGridMainItem>
-        <StyledGridMainItem item>
-          <Grid container direction="column" spacing={2}>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/aboutUs"
-              onClick={() => setValue(3)}
-            >
-              About Us
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/history"
-              onClick={() => setValue(3)}
-            >
-              Hisotry
-            </StyledGridItems>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/team"
-              onClick={() => setValue(3)}
-            >
-              Team
-            </StyledGridItems>
-          </Grid>
-        </StyledGridMainItem>
-        <StyledGridMainItem item>
-          <Grid container direction="column" spacing={2}>
-            <StyledGridItems
-              item
-              component={Link}
-              to="/contactUs"
-              onClick={() => setValue(4)}
-            >
-              Contact Us
-            </StyledGridItems>
-          </Grid>
-        </StyledGridMainItem>
+        <Hidden mdDown>
+          <StyledGridMainItem item>
+            <Grid container direction="column" spacing={2}>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/"
+                onClick={() => setValue(0)}
+              >
+                Home
+              </StyledGridItems>
+            </Grid>
+          </StyledGridMainItem>
+          <StyledGridMainItem item>
+            <Grid container direction="column" spacing={2}>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/services"
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(0);
+                }}
+              >
+                Services
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/customSoftware"
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(1);
+                }}
+              >
+                Custom Software Development
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/mobileApp"
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(2);
+                }}
+              >
+                Mobile App Development
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/websites"
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(3);
+                }}
+              >
+                Websites Development
+              </StyledGridItems>
+            </Grid>
+          </StyledGridMainItem>
+          <StyledGridMainItem item>
+            <Grid container direction="column" spacing={2}>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/theRevolution"
+                onClick={() => setValue(2)}
+              >
+                The Revolution
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/vision"
+                onClick={() => setValue(2)}
+              >
+                Vision
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/technology"
+                onClick={() => setValue(2)}
+              >
+                Technology
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/process"
+                onClick={() => setValue(2)}
+              >
+                Process
+              </StyledGridItems>
+            </Grid>
+          </StyledGridMainItem>
+          <StyledGridMainItem item>
+            <Grid container direction="column" spacing={2}>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/aboutUs"
+                onClick={() => setValue(3)}
+              >
+                About Us
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/history"
+                onClick={() => setValue(3)}
+              >
+                Hisotry
+              </StyledGridItems>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/team"
+                onClick={() => setValue(3)}
+              >
+                Team
+              </StyledGridItems>
+            </Grid>
+          </StyledGridMainItem>
+          <StyledGridMainItem item>
+            <Grid container direction="column" spacing={2}>
+              <StyledGridItems
+                item
+                component={Link}
+                to="/contactUs"
+                onClick={() => setValue(4)}
+              >
+                Contact Us
+              </StyledGridItems>
+            </Grid>
+          </StyledGridMainItem>
+        </Hidden>
       </StyledGridMain>
       <StyledImage alt="black decorative" src={footerAdornment} />
+      <Grid
+        container
+        sx={{ position: "absolute", marginTop: "-6em", right: "1.5em" }}
+        justifyContent="flex-end"
+        spacing={2}
+      >
+        <Grid
+          item
+          component={"a"}
+          href="http://www.facebook.com"
+          rel="noopener noreferrer"
+          target="blank"
+        >
+          <SocialMediaIcon alt="facebook logo" src={Facebook} />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="http://www.twitter.com"
+          rel="noopener noreferrer"
+          target="blank"
+        >
+          <SocialMediaIcon alt="twitter logo" src={twitter} />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="http://www.instagram.com"
+          rel="noopener noreferrer"
+          target="blank"
+        >
+          <SocialMediaIcon alt="instagram logo" src={Instagram} />
+        </Grid>
+      </Grid>
     </StyledFooter>
   );
 };
